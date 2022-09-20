2d4ea659bca65e06c762226913261765fd6e08cc
Nelson Drozd <nelsondrozd@gmail.com>
2022-09-20 17:33:37 -0400

    Created functions for adding manager, intern, and engineer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-const Manager = require("./Lib/manager");
+const Manager = require("./Lib/manager.js");
+const Engineer = require("./Lib/engineer.js")
 
 const employeeArray = [];
 
@@ -33,6 +34,66 @@ function createManager() {
     })
 }
 
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the manager's github profile?"
+        }
+    ]).then(answers => {
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        console.log(newEngineer)
+        employeeArray.push(newEngineer);
+        mainMenu();
+    })
+}
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the intern's school?"
+        }
+    ]).then(answers => {
+        const newintern = new intern(answers.name, answers.id, answers.email, answers.github)
+        console.log(newintern)
+        employeeArray.push(newintern);
+        mainMenu();
+    })
+}
+
 function mainMenu() {
     inquirer.prompt({
         type: "list",
@@ -41,13 +102,13 @@ function mainMenu() {
         choices: [
             "Add Engineer",
             "Add an Intern",
-            "Finish Building"
+            "Finish"
         ]
     }).then(answer => {
         if(answer.choice == "Add Engineer") {
-            // you call the createEngineer 
+            createEngineer()
         } else if (answer.choice == "Add an Intern") {
-            // createIntern
+            createIntern()
         } else {
             // call the function that will generate the HTML using the employeeArray that you have filled so far
             
